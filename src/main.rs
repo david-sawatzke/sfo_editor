978bb52a0f4c8b2ea676379bafa1c6afbe88ed05
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-06-23 14:42:39 +0200

    Add cmd interface & write functionality

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,54 @@
 // - https://www.psdevwiki.com/ps3/Eboot.PBP#PARAM.SFO
 // - https://www.psdevwiki.com/ps4/Param.sfo
 
-//use std::str::{from_utf8, Utf8Error};
+use std::collections::BTreeMap;
+use std::fmt;
+use std::fs;
+use std::mem::size_of;
+use std::num::ParseIntError;
+use std::path::PathBuf;
+
 use byteorder::{ByteOrder, LittleEndian};
 use scroll::{Pread, LE};
 use scroll_derive::Pread;
-use std::fs;
-use std::mem::size_of;
+use structopt::StructOpt;
 
+#[derive(StructOpt, Debug, PartialEq, Eq)]
+#[structopt(name = "sfo_editor")]
+struct Opt {
+    /// Activate debug mode
+    #[structopt(short = "d", long = "debug")]
+    debug: bool,
+    /// Sfo file
+    #[structopt(parse(from_os_str))]
+    file: PathBuf,
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+fn parse_hex(src: &str) -> Result<u32, ParseIntError> {
+    if src.starts_with("0x") == false {
+        // This is evil & bad, but I'm lazy
+        u32::from_str_radix("INVALID NUMBER", 16)?;
+    }
+    u32::from_str_radix(&src[2..], 16)
+}
+
+#[derive(StructOpt, Debug, PartialEq, Eq)]
+enum Command {
+    /// Simply output the fields
+    #[structopt(name = "read")]
+    Read,
+    /// Set a Integer parameter
+    #[structopt(name = "write")]
+    Write {
+        /// parameter name
+        name: String,
+        /// parameter value
+        #[structopt(parse(try_from_str = "parse_hex"))]
+        value: u32,
+    },
+}
 #[derive(Pread, Debug, PartialEq, Eq)]
 #[repr(C)]
 pub struct Header {
@@ -28,12 +69,26 @@ pub enum EntryData {
     Integer(u32),
 }
 
+impl fmt::Display for EntryData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            EntryData::Utf8(string) => write!(f, "\"{}\"", string),
+            EntryData::Integer(i) => write!(f, "{:#010x}", i),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct Entry {
     pub name: String,
     pub index_table_entry: SfoIndexTableEntry,
     pub num: u32,
     pub data: EntryData,
+}
+impl fmt::Display for Entry {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.name, self.data)
+    }
 }
 
 #[derive(Pread, Debug, PartialEq, Eq)]
@@ -47,9 +102,10 @@ pub struct SfoIndexTableEntry {
 }
 
 fn main() {
-    let bytes = fs::read("./PARAM.SFO").unwrap();
+    let opt = Opt::from_args();
+    let mut bytes = fs::read(&opt.file).unwrap();
     let data = bytes.pread_with::<Header>(0, LE).unwrap();
-    let index: Vec<Entry> = (0..data.table_entries)
+    let index: BTreeMap<String, Entry> = (0..data.table_entries)
         .map(|num| {
             (
                 num,
@@ -82,16 +138,34 @@ fn main() {
                 _ => panic!("Undefined data pattern"),
             };
 
-            Entry {
-                name,
-                index_table_entry: entry,
-                data,
-                num,
-            }
+            (
+                name.clone(),
+                Entry {
+                    name,
+                    index_table_entry: entry,
+                    data,
+                    num,
+                },
+            )
         })
         .collect();
 
     let data = bytes.pread_with::<Header>(0, LE).unwrap();
-    println!("{:#x?}", data);
-    println!("{:#x?}", index);
+    if opt.debug == true {
+        println!("{:#x?}", data);
+        println!("{:#x?}", index);
+    }
+    match opt.cmd {
+        Command::Read => {
+            for entry in index.values() {
+                println!("{}", entry);
+            }
+        }
+        Command::Write { name, value } => {
+            let entry = index.get(&name).expect("Couldn't find entry with key");
+            let offset = (data.data_table_start + (*entry).index_table_entry.data_offset) as usize;
+            LittleEndian::write_u32(&mut bytes[offset..offset + 4], value);
+        }
+    }
+    fs::write(&opt.file, bytes).unwrap();
 }
